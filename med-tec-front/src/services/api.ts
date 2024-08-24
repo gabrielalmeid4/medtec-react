569e07f7a62a2c89d3e33dc4d07a48c5eb5efa2f
Gabriel de Almeida <126924497+gabrielalmeid4@users.noreply.github.com>
2024-08-24 11:37:33 -0300

    versão 1.0 do frontend

--- med-tec-front/src/services/api.ts
+++ med-tec-front/src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Altere para o URL da sua API
+  baseURL: 'http://localhost:3000',
 });
 
-export default api;
+export default api
